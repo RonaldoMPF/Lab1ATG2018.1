f3c965cf22b0ed87f0a049be6d500da9dbbe4ae1
RonnanSouza <ronanpalmeiras@gmail.com>
2018-04-30 23:07:51 -0300

    Modifica caminho do arquivo data.txt dentro do Main.java, add arquivos ao gitignore

--- Lab1ATG/src/graphLibrary/Main.java
+++ Lab1ATG/src/graphLibrary/Main.java
@@ -1,13 +1,15 @@
 package graphLibrary;
 
+import java.io.File;
 import java.io.IOException;
 
 public class Main {
 
 	public static void main(String[] args) {
-		
+		String basePath = new File("Lab1ATG/src/graphLibrary/data.txt").getAbsolutePath();
+
 		try {
-			Graph<Integer> graph = GraphReader.readGraph("/home/klynger/dev/Lab1ATG2018.1/Lab1ATG/src/graphLibrary/data.txt");
+			Graph<Integer> graph = GraphReader.readGraph(basePath);
 			System.out.println(graph.graphRepresentation(Representation.AL));
 		} catch (IOException e) {
 			e.printStackTrace();
